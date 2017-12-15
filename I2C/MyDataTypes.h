39e5e2bafa70376cf4b9c01ba191d6bc3442617f
javad123javad <javad123javad@yahoo.com>
2017-12-15 09:59:24 +0330

    My custom data types.

---
+++ I2C/MyDataTypes.h
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   MyDataTypes.h
+ * Author: javad
+ *
+ * Created on July 30, 2017, 8:56 PM
+ */
+
+#ifndef MYDATATYPES_H
+#define MYDATATYPES_H
+#include <stdint.h>
+
+/* Success/ Failure Definition*/
+
+#define SUCCESS    0
+#define FAILURE    -1
+
+/* Custom Data Type definition*/
+
+typedef uint8_t     u8;
+typedef int8_t      s8;
+typedef uint16_t    u16;
+typedef int16_t     s16;
+typedef uint32_t    u32;
+typedef int32_t     s32;
+typedef uint64_t    u64;
+typedef int64_t     s64;
+
+
+#endif /* MYDATATYPES_H */
